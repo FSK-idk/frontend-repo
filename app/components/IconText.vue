06b9a919cb8594e1a2cc740d623eeba4227fb8bb
fsk <vovarybkin666@gmail.com>
2025-11-13 10:58:40 +1000

    hw4

---
+++ app/components/IconText.vue
@@ -0,0 +1,28 @@
+<script setup>
+const base = import.meta.env.BASE_URL;
+
+defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+<template>
+  <div class="container">
+    <img :src="`${base}assets/images/${src}`" />
+    <div><slot /></div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@use "assets/scss/variables" as *;
+@use "assets/scss/mixins" as mixins;
+
+.container {
+  @include mixins.flex-row;
+  gap: 8px;
+  color: $dark-slate-gray;
+  font-family: $font-title;
+  font-size: 14px;
+}
+</style>
