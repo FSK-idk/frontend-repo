31f2778a66364ef06e4c2d790ec6c2ac087ae288
fsk <vovarybkin666@gmail.com>
2025-11-19 17:21:10 +1000

    remove unnecessary div

--- app/components/Button.vue
+++ app/components/Button.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="button">
-    <button><slot /></button>
-  </div>
+  <button class="button"><slot /></button>
 </template>
 <style lang="scss" scoped>
 @use "assets/scss/variables" as *;
